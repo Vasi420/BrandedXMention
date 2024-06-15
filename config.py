65c05ffd45abf00f1af7f59a647a2e8ea619e0f8
Vasi <157385207+Vasi420@users.noreply.github.com>
2024-06-15 18:45:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "10046731"
+# -------------------------------------------------------------
+API_HASH = "09a25ea3e558df7e5d969c144b0452d9"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = getenv("OWNER_ID", "5981008313")
+SUPPORT_GRP = "the_comedy_bar"
+UPDATE_CHNL = "AS_Bots_Updates"
+OWNER_USERNAME = "Oxygen_420"
